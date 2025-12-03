ff2f3dd4d328970e81547a08c2e6680535fbbf15
daon217 <daon217@gmail.com>
2025-12-03 21:21:14 +0900

    1203

---
+++ user/src/test/java/edu/sm/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sm.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
